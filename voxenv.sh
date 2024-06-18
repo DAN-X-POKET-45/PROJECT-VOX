adeed1c0a9d791010735c96b787ca194ea5caf65
Daniel-poket-45 <daniel12001684@outlook.com>
2024-06-17 21:10:18 -0300

    CLI VOX EDITOR WITH TMUX

---
+++ voxenv.sh
@@ -0,0 +1,20 @@
+#Variáveis de ambiente
+export PROJECT_VOX_PATH=~/PROJECT-VOX
+export PROJECT_VOX_ENV_VERSION=1.0
+export 
+
+#Entrada de diretórios
+cd $PROJECT_VOX_PATH
+
+#Inicializaçao do tmux
+tmux new-session -d -s voxadm
+
+#Criação das janelas
+tmux rename-window -t voxadm:0  "PROJECT-VOX EDITOR"
+tmux split-window -v -t voxadm:0 -d
+
+#Seleção de layout
+tmux select-layout -t voxadm:0 main-horizontal
+
+#Vinculação ao tmux
+tmux attach-session -t voxadm
