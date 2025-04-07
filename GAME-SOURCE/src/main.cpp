ba99adf6fb0f2533921d44bee8712b0c6a102836
Daniel-poket-45 <daniel12001684@outlook.com>
2025-04-06 22:47:15 -0300

    imgui simplification

--- GAME-SOURCE/src/main.cpp
+++ GAME-SOURCE/src/main.cpp
@@ -1,54 +1,55 @@
 #include <iostream>
 #include <GL/gl.h>
-#include "window/window.hpp"
-#include "libs/DearImGui/imgui.h"
-#include "libs/DearImGui/imgui_impl_glfw.h"
-#include "libs/DearImGui/imgui_impl_opengl3.h"
+#include <window.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 int main(){
-    window game_window(600, 400, "VOX TEST");
+    window game_window(600, 400, "VOX TEST"); //Objeto de janela personalizado
 
-    // Configurar o Dear ImGui
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    ImGui::StyleColorsDark(); // Estilo padrão
+    //Configurar o Dear ImGui
+    IMGUI_CHECKVERSION(); //checar versão do ImGui
+    ImGui::CreateContext(); //Criar contexto do ImGui
+    //ImGuiIO& io = ImGui::GetIO(); //Configurar IO do ImGui
+    ImGui::StyleColorsDark(); //Estilo de cor para o ImGui
 
-    // Configurar os backends
+    //Configurar os backends GLFW e OpenGL para o ImGui
     ImGui_ImplGlfw_InitForOpenGL(game_window.glfw_window, true);
     ImGui_ImplOpenGL3_Init("#version 420");
 
     
+
+    //enquanto a janela não receber um sinal de fechamento
     while(!game_window.should_close()){
         //Processa eventos de entrada
         game_window.poll_events();
 
-        // Iniciar frame do ImGui
+        //Iniciar frame do ImGui
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // Mostrar a Demo Window
-        bool show_demo_window = true;
-        ImGui::ShowDemoWindow(&show_demo_window);
+        //Mostrar a Demo Window
+        ImGui::ShowDemoWindow();
 
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(game_window.glfw_window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
-        // Limpar a tela com uma cor de fundo
-        glClearColor(1.0f, 0.5f, 0.5f, 1.0f); 
-        glClear(GL_COLOR_BUFFER_BIT);
 
+        //Limpar a tela com uma cor de fundo
+        glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         //Troca os buffers front e back
         game_window.swap_buffers();
 
-        //janela da aplicação finalizada
+        //Janela da aplicação finalizada
     }
 
-    // Limpeza
+    //Limpeza
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
